62051f18c9d2d6becf30bfd7d5038425ce0cd480
Takahiro Ikeda <ikeadless@gmail.com>
2015-07-31 23:11:41 +0900

    add validate of post name, comment

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,7 +8,7 @@ class PostsController < ApplicationController
     if @post.save
       redirect_to posts_path, notice: 'post was saved'
     else
-      render new
+      render :new 
     end
   end
 
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,4 @@
 class Post < ActiveRecord::Base
+  validates :name, presence: true
+  validates :comment, presence: true
 end
